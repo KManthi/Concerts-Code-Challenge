bc607e0878eaefbae53fec3d8e3db062a1a64f92
Daniel <dani3lkamanthi@gmail.com>
2024-06-06 23:10:11 +0300

    Completes properties

--- lib/classes/many_to_many.py
+++ lib/classes/many_to_many.py
@@ -3,6 +3,31 @@ class Band:
         self.name = name
         self.hometown = hometown
 
+    @property
+    def name(self):
+        return self._name
+    
+    @name.setter
+    def name(self, name):
+        if isinstance(name, str) and len(name) > 0:
+            self._name = name
+        else:
+            raise ValueError("Band name must be a non-empty string")
+        
+    @property
+    def hometown(self):
+        return self._hometown
+    
+    @hometown.setter
+    def hometown(self, hometown):
+        if  not isinstance(hometown, str) and len(hometown) > 0:
+            raise ValueError('Band homwtown must be a non-empty string')
+        if hasattr(self, '_hometown'):
+            raise AttributeError('Band hometown cannot be changed')
+        self._hometown = hometown
+
+
+
     def concerts(self):
         pass
 
@@ -22,6 +47,17 @@ class Concert:
         self.band = band
         self.venue = venue
 
+    @property
+    def date(self):
+        return self._date
+    
+    @date.setter
+    def date(self, date):
+        if isinstance(date, str) and len(date) > 0:
+            self._date = date
+        else:
+            raise ValueError("Concert date must be a non-empty string")
+
     def hometown_show(self):
         pass
 
@@ -33,6 +69,28 @@ class Venue:
     def __init__(self, name, city):
         self.name = name
         self.city = city
+
+    @property
+    def name(self):
+        return self._name
+    
+    @name.setter
+    def name(self, name):
+         if isinstance(name, str) and len(name) > 0:
+            self._name = name
+         else:
+            raise ValueError("Venue name must be a non-empty string")
+         
+    @property
+    def city(self):
+        return self._city
+    
+    @city.setter
+    def city(self, city):
+        if isinstance(city, str) and len(city) > 0:
+            self._city = city
+        else:
+            raise ValueError("City name must be a non-empty string")
 
     def concerts(self):
         pass
